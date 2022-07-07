973c39e8e8a6016ed373e6c8918ee510c913270a
Nodiru Gaji <c0d3r.nodiru.gaji@gmail.com>
2022-07-08 01:56:40 +0300

    Add nav to the base template

--- src/blueprints/root/main.py
+++ src/blueprints/root/main.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
 
 
-root = Blueprint('root', __name__, static_folder='static', template_folder='templates')
+root = Blueprint(
+  'root', __name__,
+  static_folder='static/root', template_folder='templates',
+  static_url_path='/static/root'
+)
